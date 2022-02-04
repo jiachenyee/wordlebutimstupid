009e0dd2ac5a8e9b9eb49c54dca6215c3a0d37f7
Jia Chen <yee_jia_chen@s2017.sst.edu.sg>
2022-02-04 21:03:58 +0800

    Initial Commit

---
+++ wordle but im stupid/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wordle but im stupid
+//
+//  Created by Jia Chen Yee on 4/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wordle but im stupid/wordle_but_im_stupidApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordle_but_im_stupidApp.swift
+//  wordle but im stupid
+//
+//  Created by Jia Chen Yee on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct wordle_but_im_stupidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
